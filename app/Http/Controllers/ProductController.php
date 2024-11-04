f2fa80e0b1c378e5e737d25337c3275a22309cdb
mahayasa1 <mahayasa.udit@gmail.com>
2024-11-04 23:25:03 +0800

    new project

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index() 
+    {
+        $products = Product::orderBy('id', 'desc')->get();
+        $total = Product::count();
+        return view('admin.product.home', compact('products', 'total'));
+    }
+
+    public function create()
+    {
+        return view('admin.product.create');
+    }
+
+    public function save(Request $request)
+    {
+        $validation = $request->validate([
+            'title' => 'required',
+            'category' => 'required',
+            'price' => 'required',
+        ]);
+
+        $data = Product::create($validation);
+        if ($data) {
+            session()->flash('success', 'Product Add Successfully');
+            return redirect(route('admin/products'));
+        }else {
+            session()->flash('error', 'some problem occure');
+            return redirect(route('admin/products/create'));
+        }
+
+    }
+
+    public function edit($id)
+    {
+        $poducts = Product::findOrFail($id);
+        return view('admin.product.update', compact('products'));
+    }
+}
